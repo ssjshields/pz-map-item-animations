46ce3b4a5287e4b77b45f7ef6c1d1e1094681631
ssjshields <88489119+ssjshields@users.noreply.github.com>
2024-05-01 23:19:51 -0400

    initial before sandbox option fix

---
+++ MapItemAnimations/Contents/mods/MapItemAnimations/media/lua/client/MapItemAnimation.lua
@@ -0,0 +1,32 @@
+require "ISUI/ISInventoryPaneContextMenu"
+--client\ISUI\ISInventoryPaneContextMenu.lua
+local old_ISInventoryPaneContextMenu_onCheckMap = ISInventoryPaneContextMenu.onCheckMap
+function ISInventoryPaneContextMenu.onCheckMap(map, player)
+    local playerObj = getSpecificPlayer(player)
+    if playerObj then
+        ISTimedActionQueue.clear(playerObj)
+        ISTimedActionQueue.add(ReadAMap:new(playerObj, map))
+    end
+end
+
+
+require "TimedActions/ISReadWorldMap"
+---@class ISReadWorldMap : ISBaseTimedAction
+ReadAMap = ISReadWorldMap:derive("ReadAMap")
+
+function ReadAMap:new(character, map)
+    local o = ISReadWorldMap.new(self, character)
+    o.map = map
+    return o
+end
+
+function ReadAMap:start()
+    self:setAnimVariable("ReadType", "newspaper")
+    self:setActionAnim(CharacterActionAnims.Read)
+    self:setOverrideHandModelsString(nil, "MapInHand")
+end
+
+function ReadAMap:perform()
+    old_ISInventoryPaneContextMenu_onCheckMap(self.map, self.character:getPlayerNum())
+    ISBaseTimedAction.perform(self)
+end
